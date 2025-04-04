e5d13b5814ac009c0585985a5797c4c40cdee3f8
Axel <68563103+Algorithman17@users.noreply.github.com>
2025-04-04 22:44:24 +0200

    Delete legal-pages-generator directory

--- legal-pages-generator/config/constants/dir.js
+++
@@ -1,8 +0,0 @@
-// Exporte un objet contenant la configuration des répertoires pour 11ty.
-export default {
-  input: 'src', // Répertoire source des fichiers.
-  output: '_site', // Répertoire de sortie généré par 11ty.
-  includes: '_includes', // Répertoire des fichiers inclus.
-  layouts: '_layouts', // Répertoire des layouts.
-  data: '_data', // Répertoire des fichiers de données.
-};
